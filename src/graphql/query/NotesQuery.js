65411c35ca43b5ba5c5d2136030ee02d35d88348
jnyjhow <jhonatan_yamane@hotmail.com>
2022-09-15 13:55:33 -0300

    setting up the back end using graphql and express.

---
+++ src/graphql/query/NotesQuery.js
@@ -0,0 +1,18 @@
+//funcao para retornar todas as notas armazenadas no banco de dados
+
+const { noteType } = require("../nodeTypes");
+const { GraphQLList } = require("graphql");
+const NoteService = require("../../services/NoteService");
+
+const NotesQuery = {
+  type: GraphQLList(noteType),
+  args: {},
+  resolve: async () => {
+    const noteService = new NoteService();
+    const notes = await noteService.getAllNotes();
+
+    return notes;
+  },
+};
+
+module.exports = { NotesQuery };

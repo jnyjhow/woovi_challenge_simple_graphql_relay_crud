45774683b92d38c152ab678cdf4fbbe54e705c81
jnyjhow <jhonatan_yamane@hotmail.com>
2022-09-15 12:46:11 -0300

    configuring database.

---
+++ src/config/databaseConnection.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+let mongoDB;
+
+const setupDB = (callback) => {
+  const uri = process.env.MONGOBD_URI;
+
+  MongoClient.connect(
+    uri,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+      mongoDB = client.db(process.env.MONGOBD_DATABASE);
+
+      if (err) {
+        return callback(err);
+      } else {
+        return callback("DB: OK");
+      }
+    }
+  );
+};
+
+const getDB = () => {
+  return mongoDB;
+};
+
+module.exports = { setupDB, getDB };

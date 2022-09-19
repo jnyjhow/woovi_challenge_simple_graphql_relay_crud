c2456d737062d2ecc6e09c395bd6f6c78d99e078
jnyjhow <jhonatan_yamane@hotmail.com>
2022-09-19 14:11:40 -0300

    changing file to realtime updates.

--- notes-frontend/src/index.js
+++ notes-frontend/src/index.js
@@ -8,6 +8,10 @@ import { BrowserRouter } from "react-router-dom";
 import { setContext } from "@apollo/client/link/context";
 import { AUTH_TOKEN } from "./constants";
 
+import { split } from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
 //importando apollo client
 import {
   ApolloProvider,
@@ -31,9 +35,29 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
   //link: httpLink,
-  link: authLink.concat(httpLink),
+  //link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache(),
 });
 
